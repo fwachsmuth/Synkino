939865f1e8f6ae088b8930f056f39fbc5a44c881
Friedemann Wachsmuth <me@peaceman.de>
2018-02-01 23:37:44 +0100

    more play mode scaffolding

--- Synkino_Frontend/Synkino_Frontend.ino
+++ Synkino_Frontend/Synkino_Frontend.ino
@@ -90,6 +90,25 @@ static const unsigned char busybee_xbm_bits[] U8X8_PROGMEM = {
    0xD8, 0x2A, 0x60, 0x07, 0xB8, 0x1A, 0xCC, 0x3F, 0x86, 0x06, 0x42, 0x7B,
    0x22, 0x1B, 0x52, 0x6A, 0x2C, 0x28 };
 
+#define play_xbm_width 9
+#define play_xbm_height 9
+static const unsigned char play_xbm_bits[] U8X8_PROGMEM = {
+   0x03, 0x00, 0x0F, 0x00, 0x3F, 0x00, 0xFF, 0x00, 0xFF, 0x01, 0xFF, 0x00,
+   0x3F, 0x00, 0x0F, 0x00, 0x03, 0x00 };
+
+#define pause_xbm_width 8
+#define pause_xbm_height 8
+static const unsigned char pause_xbm_bits[] U8X8_PROGMEM = {
+   0xE7, 0xE7, 0xE7, 0xE7, 0xE7, 0xE7, 0xE7, 0xE7 };
+
+#define sync_xbm_width 20
+#define sync_xbm_height 9
+static const unsigned char sync_xbm_bits[] U8X8_PROGMEM = {
+   0xFE, 0xFF, 0x07, 0xFF, 0xFF, 0x0F, 0xA7, 0xDA, 0x0C, 0xBB, 0x52, 0x0F,
+   0x77, 0x4B, 0x0F, 0x6F, 0x5B, 0x0F, 0x73, 0xDB, 0x0C, 0xFF, 0xFF, 0x0F,
+   0xFE, 0xFF, 0x07 };
+
+
 
 // ---- Initialize Objects ---------------------------------------------------------
 //
@@ -275,12 +294,16 @@ void drawPlayingMenu() {
     u8g2.setFont(u8g2_font_helvR08_tr);
     u8g2.drawStr(0,8,"Bauer t610");
     u8g2.drawStr(90,8,"Film 008");
+    u8g2.drawStr(98,62,"18 fps");
     u8g2.setFont(u8g2_font_inb24_mn);
     u8g2.drawStr(20,36,":");
     u8g2.drawStr(71,36,":");
     u8g2.drawStr( 4,40,"0");
     u8g2.drawStr(35,40,"08");
     u8g2.drawStr(85,40,"23");
+    u8g2.drawXBMP(60, 54, play_xbm_width, play_xbm_height, play_xbm_bits);
+    // u8g2.drawXBMP(60, 54, pause_xbm_width, pause_xbm_height, pause_xbm_bits);
+    u8g2.drawXBMP(2, 54, sync_xbm_width, sync_xbm_height, sync_xbm_bits);
   } while ( u8g2.nextPage() );
 }
 void drawBusyBee(byte x, byte y) {
@@ -319,7 +342,7 @@ uint16_t selectTrackScreen() {
       } else {
         do {
           u8g2.setFont(u8g2_font_inb46_mn);
-          u8g2.setCursor(8, 60);
+          u8g2.setCursor(8, 55);
           if (newEncPosition < 10)  u8g2.print("0");
           if (newEncPosition < 100) u8g2.print("0");
           u8g2.print(newEncPosition);
