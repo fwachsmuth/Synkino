46822c922c7d98498c0f920b4b8d4445993cb7ca
Friedemann Wachsmuth <me@peaceman.de>
2018-06-24 01:28:18 +0200

    More todos

--- Synkino_Frontend/Synkino_Frontend.ino
+++ Synkino_Frontend/Synkino_Frontend.ino
@@ -17,6 +17,7 @@ const char *dspVersion = "DSP v1.0";
  *  *** Bugs ***
  *  [ ] Projector Name is truncated after editing values
  *  [ ] Do not blink while characters change
+ *  [ ] Beep on String Edit Exit
  *  [ ] Fix literals in myEnc.write for 15-dent encoder
  *  [ ] Small Offset adds up after multiple projector stops
  *  [ ] Find & Load other files than just m4a
