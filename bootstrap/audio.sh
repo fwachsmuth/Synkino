1235e6badffff39b4aadc422c7ca2590896f0928
Friedemann Wachsmuth <me@peaceman.de>
2019-02-08 18:10:33 +0100

    Fixing the bootstrap scripts, by using a hex file containing a bootloader. Doh. DOn't trust file sizes!

--- bootstrap/audio.sh
+++ bootstrap/audio.sh
@@ -13,41 +13,41 @@
 -Uhfuse:w:0xD6:m \
 -Ulfuse:w:0xF7:m 
 
-# writing Optiboot
-/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
--C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
--v -patmega328p \
--cstk500v2 \
--Pusb \
--Uflash:w:/Users/peaceman/Library/Arduino15/packages/Optiboot/hardware/avr/0.6.2/bootloaders/optiboot/optiboot_atmega328.hex:i \
--Ulock:w:0x0F:m 
+# # writing Optiboot
+# /Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
+# -C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
+# -v -patmega328p \
+# -cstk500v2 \
+# -Pusb \
+# -Uflash:w:/Users/peaceman/Library/Arduino15/packages/Optiboot/hardware/avr/0.6.2/bootloaders/optiboot/optiboot_atmega328.hex:i \
+# -Ulock:w:0x0F:m
 
-# Burn Audio Code
+# Burn Audio Code & bootloader
 /Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
 	-C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
 	-v \
 	-patmega328p \
 	-cstk500v2 \
 	-Pusb \
-	-Uflash:w:Synkino_Audio_1.1.hex:i
+	-Uflash:w:/Users/peaceman/code/Arduino/Synkino/bootstrap/Synkino_Audio_1.1.hex:i
 
 
-# writing Fuses (2nd attempt)
-/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
--C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
--v -patmega328p \
--cstk500v2 \
--Pusb -e \
--Ulock:w:0x2F:m \
--Uefuse:w:0xFD:m \
--Uhfuse:w:0xD6:m \
--Ulfuse:w:0xF7:m 
-
-# writing Optiboot again
-/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
--C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
--v -patmega328p \
--cstk500v2 \
--Pusb \
--Uflash:w:/Users/peaceman/Library/Arduino15/packages/Optiboot/hardware/avr/0.6.2/bootloaders/optiboot/optiboot_atmega328.hex:i \
--Ulock:w:0x0F:m 
+# # writing Fuses (2nd attempt)
+# /Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
+# -C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
+# -v -patmega328p \
+# -cstk500v2 \
+# -Pusb -e \
+# -Ulock:w:0x2F:m \
+# -Uefuse:w:0xFD:m \
+# -Uhfuse:w:0xD6:m \
+# -Ulfuse:w:0xF7:m
+#
+# # writing Optiboot again
+# /Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
+# -C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
+# -v -patmega328p \
+# -cstk500v2 \
+# -Pusb \
+# -Uflash:w:/Users/peaceman/Library/Arduino15/packages/Optiboot/hardware/avr/0.6.2/bootloaders/optiboot/optiboot_atmega328.hex:i \
+# -Ulock:w:0x0F:m
--- bootstrap/frontend.sh
+++ bootstrap/frontend.sh
@@ -14,39 +14,39 @@
 -Ulfuse:w:0xF7:m 
 
 # writing Optiboot
-/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
--C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
--v -patmega328p \
--cstk500v2 \
--Pusb \
--Uflash:w:/Users/peaceman/Library/Arduino15/packages/Optiboot/hardware/avr/0.6.2/bootloaders/optiboot/optiboot_atmega328.hex:i \
--Ulock:w:0x0F:m 
+# /Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
+# -C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
+# -v -patmega328p \
+# -cstk500v2 \
+# -Pusb \
+# -Uflash:w:/Users/peaceman/Library/Arduino15/packages/Optiboot/hardware/avr/0.6.2/bootloaders/optiboot/optiboot_atmega328.hex:i \
+# -Ulock:w:0x0F:m
 
-# Burn Frontend Code
+# Burn Frontend Code & bootloader
 /Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
 	-C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
 	-v \
 	-patmega328p \
 	-cstk500v2 \
 	-Pusb \
-	-Uflash:w:Synkino_Frontend_1.1.hex:i
+	-Uflash:w:/Users/peaceman/code/Arduino/Synkino/bootstrap/Synkino_Frontend_1.1.hex:i
 
 # writing Fuses (2nd attempt)
-/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
--C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
--v -patmega328p \
--cstk500v2 \
--Pusb -e \
--Ulock:w:0x2F:m \
--Uefuse:w:0xFD:m \
--Uhfuse:w:0xD6:m \
--Ulfuse:w:0xF7:m 
-
-# writing Optiboot again
-/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
--C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
--v -patmega328p \
--cstk500v2 \
--Pusb \
--Uflash:w:/Users/peaceman/Library/Arduino15/packages/Optiboot/hardware/avr/0.6.2/bootloaders/optiboot/optiboot_atmega328.hex:i \
--Ulock:w:0x0F:m 
+# /Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
+# -C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
+# -v -patmega328p \
+# -cstk500v2 \
+# -Pusb -e \
+# -Ulock:w:0x2F:m \
+# -Uefuse:w:0xFD:m \
+# -Uhfuse:w:0xD6:m \
+# -Ulfuse:w:0xF7:m
+#
+# # writing Optiboot again
+# /Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude \
+# -C/Users/peaceman/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf \
+# -v -patmega328p \
+# -cstk500v2 \
+# -Pusb \
+# -Uflash:w:/Users/peaceman/Library/Arduino15/packages/Optiboot/hardware/avr/0.6.2/bootloaders/optiboot/optiboot_atmega328.hex:i \
+# -Ulock:w:0x0F:m
